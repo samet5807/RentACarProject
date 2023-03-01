b1c729c42aeb3606a1480758a23d014f5e987601
SAMET PC <samet.koray58@gmail.com>
2023-03-01 23:32:09 +0300

    EntityframeWork Eklenmesi

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -1,5 +1,4 @@
-﻿using Entities.Concrete;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,9 +6,7 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Abstract
 {
-    public interface ICarDal:IEntityRepository<Car>
+    internal interface IEntityRepository
     {
-
-
     }
 }
